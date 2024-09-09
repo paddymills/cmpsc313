542ee629d404beb7a228b5af882df59b1332a4c2
Patrick Miller <paddymills@proton.me>
2024-09-08 21:01:49 -0400

    complete evenodd

---
+++ src/hw1/evenodd.asm
@@ -0,0 +1,54 @@
+
+# TODO: documentation
+
+.data
+	inputPrompt: .asciiz "Please enter a number: "
+	isEven: .asciiz "EVEN\n"
+	isOdd: .asciiz "ODD\n"
+
+.text
+.globl main
+.ent main
+main:
+	li $v0, 4
+	la $a0, inputPrompt
+	syscall
+
+	# read integer
+	li $v0, 5
+	syscall
+
+	# store number in a temp register
+	move $t0, $v0
+
+	# set up to show answer (so that we dont' have to do it twice in branches)
+	li $v0, 4
+
+	# check if LSB is even or odd
+	#	because the immediate value is 0000..001, all except the LSB will be 0
+	#	the resulting number will be what the LSB is
+	#		0 -> even
+	#		1 -> odd
+	andi $t1, $t0, 1
+
+	# if result is 0, number is even
+	beqz $t1, even
+	# else, go to odd
+	j odd
+
+even:
+	la $a0, isEven
+	j end
+
+odd:
+	la $a0, isOdd
+
+end:
+	# show answer
+	syscall
+
+	# end
+	li $v0, 10	# end program
+	syscall
+
+.end main
