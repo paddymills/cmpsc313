3ab946a2db8aef0c3ca13212c9cd7f348c823c73
Patrick Miller <paddymills@proton.me>
2024-09-10 22:41:22 -0400

    start volume

--- src/hw1/volume.asm
+++ src/hw1/volume.asm
@@ -6,11 +6,23 @@
 # value for the radius of the sphere, output an error message. Assume the radius
 # of the sphere is a floating-point number.
 
+# Volume
+#    4               3
+#   --- * pi * radius
+#    3
+
 # ----------------------------------------
 # Data declarations go in this section.
 .data
 # program specific data declarations
+pi: .float 3.14159
+radius: .float 0.0
 
+inputPrompt: .asciiz "Enter the radius of the sphere: "
+outputLabel: .asciiz "The volume of the spere is"
+
+isNegative: .asciiz "Error: a sphere cannot have a negative radius"
+newline: .asciiz "\n"
 
 # ----------------------------------------
 # Program code goes in this section.
@@ -19,14 +31,34 @@
 .globl main
 .ent main
 main:
-
 # -----
-# your program code goes here.
+	# ask for radius
+	li $v0, 4
+	la $a0, inputPrompt
+	syscall
 
+	# read float
+	li $v0, 6
+	syscall
 
-# -----
-# Done, terminate program.
-li $v0, 10
-syscall # all done!
+	bltz $f0, error
+
+calc:
+	j end
+
+error:
+	li $v0, 4
+	la $a0, isNegative
+	syscall
+	j end
+
+end:
+	li $v0, 4
+	la $a0, newline
+	syscall
+	# -----
+	# Done, terminate program.
+	li $v0, 10
+	syscall # all done!
 
 .end main
