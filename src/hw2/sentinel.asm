b359b505e7da03fb50077dd9a0b11b8695fb8271
Patrick Miller <paddymills@proton.me>
2024-10-01 23:59:07 -0400

    complete sentinel

--- src/hw2/sentinel.asm
+++ src/hw2/sentinel.asm
@@ -18,6 +18,11 @@
 # Data Declarations:
 .data
 
+inputMessage: .asciiz "Enter a temperature (or -99 to quit): "
+avgMessage: .asciiz "The average of the temperatures is "
+sentinel: .word -99
+newline: .asciiz "\n"
+
 # ----------------------------------------
 # Program Code:
 .text
@@ -28,7 +33,41 @@ main:
 # -----
 # Program start
 
+    lw $t0, sentinel
+    li $t1, 0       # number of temperatures collected
+    li $t2, 0       # sum of temperatures collected
 
+loop:
+    li $v0,4        # ask for temperature
+    la $a0,inputMessage
+    syscall
+    li $v0,5        # read int into $v0
+    syscall
+
+    beq $v0, $t0, print # temperature == sentinel
+
+    addi $t1, $t1, 1    # increment temperature counter
+    add $t2, $t2, $v0   # add temperature to running total
+
+    b loop
+
+print:
+    # calculate average ($t2/$t1)
+    mtc1 $t1, $f2       # load number of temperatures into float register
+    cvt.s.w $f2, $f2    # convert number of temperatures to float
+    mtc1 $t2, $f4       # load sum of temperatures into float register
+    cvt.s.w $f4, $f4    # convert sum of temperatures to float
+    div.s $f6, $f4, $f2 # calculate!
+
+    li $v0,4            # print output message
+    la $a0,avgMessage
+    syscall
+    li $v0,2            # print float
+    mov.s $f12, $f6
+    syscall
+    li $v0,4            # print newline
+    la $a0,newline
+    syscall
 
 # -----
 # Done, terminate program.
