84ae787f4343c47d535a6c8bc839394c2fd1444c
Patrick Miller <paddymills@proton.me>
2024-10-01 23:39:48 -0400

    completed pattern

--- src/hw2/pattern.asm
+++ src/hw2/pattern.asm
@@ -20,6 +20,12 @@
 # Data Declarations:
 .data
 
+inputMessage: .asciiz "Pattern length: "
+invalidMessage: .asciiz "Length must be greater than 0\n"
+
+star: .asciiz "*"
+newline: .asciiz "\n"
+
 # ----------------------------------------
 # Program Code:
 .text
@@ -29,8 +35,53 @@
 main:
 # -----
 # Program start
+    li $v0,4        # ask for number
+    la $a0,inputMessage
+    syscall
+    li $v0,5        # read number
+    syscall
+    move $t0,$v0    # store number
 
+    blez $t0,invalid
 
+    li $t1,1        # iteration counter (line length)
+    li $t2,1        # movement direction (increase/decrease)
+loop:
+    li $t3,0        # for counting number of stars printed
+printLoop:
+    # exit if line length == stars printed
+    beq $t1, $t3, endPrintLoop
+
+    li $v0,4        # print star
+    la $a0,star
+    syscall
+
+    add $t3, $t3, 1 # increment star counter
+    b printLoop
+
+endPrintLoop:
+    li $v0,4            # print newline
+    la $a0,newline
+    syscall
+
+    add $t1, $t1, $t2   # increment iteration counter
+
+    # if line length == user number, reverse direction
+    beq $t1, $t0, reverseDirection
+    # else, next
+    b nextIteration
+
+reverseDirection:
+    li $t2,-1   # start descending pattern
+nextIteration:
+    beqz $t1, end
+    b loop
+
+invalid:
+    li $v0,4    # report invalid number
+    la $a0,invalidMessage
+    syscall
+    b main      # retry input
 
 # -----
 # Done, terminate program.
