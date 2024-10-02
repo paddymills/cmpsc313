86819141d7794129b6d9fbb61540ed21b9c85104
Patrick Miller <paddymills@proton.me>
2024-10-02 14:15:58 -0400

    fix indent

--- src/hw2/pattern.asm
+++ src/hw2/pattern.asm
@@ -35,58 +35,58 @@ newline: .asciiz "\n"
 main:
 # -----
 # Program start
-    li $v0,4        # ask for number
-    la $a0,inputMessage
-    syscall
-    li $v0,5        # read number
-    syscall
-    move $t0,$v0    # store number
+	li $v0,4        # ask for number
+	la $a0,inputMessage
+	syscall
+	li $v0,5        # read number
+	syscall
+	move $t0,$v0    # store number
 
-    blez $t0,invalid
+	blez $t0,invalid
 
-    li $t1,1        # iteration counter (line length)
-    li $t2,1        # movement direction (increase/decrease)
+	li $t1,1        # iteration counter (line length)
+	li $t2,1        # movement direction (increase/decrease)
 loop:
-    li $t3,0        # for counting number of stars printed
+	li $t3,0        # for counting number of stars printed
 printLoop:
-    # exit if line length == stars printed
-    beq $t1, $t3, endPrintLoop
+	# exit if line length == stars printed
+	beq $t1, $t3, endPrintLoop
 
-    li $v0,4        # print star
-    la $a0,star
-    syscall
+	li $v0,4        # print star
+	la $a0,star
+	syscall
 
-    add $t3, $t3, 1 # increment star counter
-    b printLoop
+	add $t3, $t3, 1 # increment star counter
+	b printLoop
 
 endPrintLoop:
-    li $v0,4            # print newline
-    la $a0,newline
-    syscall
+	li $v0,4            # print newline
+	la $a0,newline
+	syscall
 
-    add $t1, $t1, $t2   # increment iteration counter
+	add $t1, $t1, $t2   # increment iteration counter
 
-    # if line length == user number, reverse direction
-    beq $t1, $t0, reverseDirection
-    # else, next
-    b nextIteration
+	# if line length == user number, reverse direction
+	beq $t1, $t0, reverseDirection
+	# else, next
+	b nextIteration
 
 reverseDirection:
-    li $t2,-1   # start descending pattern
+	li $t2,-1   # start descending pattern
 nextIteration:
-    beqz $t1, end
-    b loop
+	beqz $t1, end
+	b loop
 
 invalid:
-    li $v0,4    # report invalid number
-    la $a0,invalidMessage
-    syscall
-    b main      # retry input
+	li $v0,4    # report invalid number
+	la $a0,invalidMessage
+	syscall
+	b main      # retry input
 
 # -----
 # Done, terminate program.
 end:
-    li $v0, 10
-    syscall
+	li $v0, 10
+	syscall
 
 .end main
