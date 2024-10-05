774f2b19cddad0cb559913b30ac22afaf267a2ff
Patrick Miller <paddymills@proton.me>
2024-10-05 09:59:15 -0400

    complete isComposite

--- src/hw3/composite.asm
+++ src/hw3/composite.asm
@@ -13,6 +13,9 @@
 # Data Declarations:
 .data
 
+message: .asciiz "The first 100 composite numbers are:\n"
+newline: .asciiz "\n"
+
 # ----------------------------------------
 # Program Code:
 .text
@@ -22,8 +25,31 @@
 main:
 # -----
 # Program start
+	li $s0, 1	# composite number counter
+	li $s1, 4	# number to check for composite
 
+	# Print message
+	li $v0, 4
+	la $a0, message
+	syscall
 
+loopFind:
+	move $a0, $s1	# number to check for composite
+	jal isComposite
+	beqz $v0, incrementNumber	# if not composite, increment counter
+
+	# number is composite
+	addi $s0, $s0, 1	# increment composite number counter
+	li $v0, 1	# print composite number
+	syscall
+
+	li $v0, 4	# print newline
+	la $a0, newline
+	syscall
+
+incrementNumber:
+	addi $s1, $s1, 1	# increment number to check
+	ble $s0, 100, loopFind	# check for 100 composite numbers
 
 # -----
 # Done, terminate program.
@@ -34,12 +60,24 @@ end:
 .end main
 
 .globl isComposite
-.main isComposite
-start:
+.ent isComposite
+isComposite:
+	li $v0, 0	# default: is not composite
+	ble $a0, 3, end	# base case: a0 <= 3 is not composite
 
-loop:
+	li $t0, 2	# number to check divisible
+loopCheckDivisible:
+	beq $a0, $t0, endIS	# if a0 == t0, stop checking
+	rem $t1, $a0, $t0	# a0 % t0
+	beqz $t1, composite	# if a0 % t0 == 0, then a0 is composite
 
-end:
+	addi $t0, $t0, 1	# increment number to check
+	b loopCheckDivisible
+
+composite:
+	li $v0, 1
+
+endIS:
 	jr $ra
 
 .end isComposite
