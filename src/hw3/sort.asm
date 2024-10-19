2c9ddc509d1a7e246fe72fb1a1e91e62af1a3365
Patrick Miller <paddymills@proton.me>
2024-10-19 16:33:08 -0400

    complete quick sort

--- src/hw3/sort.asm
+++ src/hw3/sort.asm
@@ -19,13 +19,10 @@
 .data
 
 getArrayLength: .asciiz "Number of elements in the array: "
-getArrayValues: .asciiz "Array values: \n"
 getArrayValuesStart: .asciiz "Array["
 getArrayValuesEnd: .asciiz "]: "
 
-outUnsortedArray: .asciiz "Unsorted array: "
-outSortedArray: .asciiz "Sorted array: "
-outArrayStart: .asciiz "["
+outArrayStart: .asciiz "Array: ["
 outArrayDelim: .asciiz ", "
 outArrayEnd: .asciiz "]\n"
 
@@ -40,11 +37,6 @@ newline: .asciiz "\n"
 main:
 # -----
 # Program start
-	# TODO: store start of array location in $s0 ($sp of first element)
-	# TODO: store length of array in $s1 (currently in $s0)
-
-	b testArray	# skip user input for testing
-
 	# get array length
 	li $v0, 4
 	la $a0, getArrayLength
@@ -53,20 +45,20 @@ main:
 	syscall
 	move $s1, $v0		# store array length
 
+	# calculate size of array
+	sll $t0, $s1, 2		# size = length * 4
+
 	# create dynamic array
 	li $v0, 9
-	move $a0, $s1
+	move $a0, $t0
 	syscall
 	move $s0, $v0		# store array start address
 
 	# get array values
 	li $t0, 0		# index counter
 	move $t1, $s0	# store array start address
-	li $v0, 4
-	la $a0, getArrayValues
-	syscall
 loopInput:
-	beq $t0, $s1, printUnsorted
+	beq $t0, $s1, sortArray	# if index == length, exit loop
 
 	# prompt: `Array[i]: `
 	li $v0, 4
@@ -82,57 +74,20 @@ loopInput:
 	# get number from user
 	li $v0, 5
 	syscall
-	sw $v0, ($t1)		# push number onto stack
+
+	sw $v0, ($t1)		# push number into array
 	addi $t1, $t1, 4	# increment array pointer
 	addi $t0, $t0, 1	# increment index counter
 
 	b loopInput
 
-testArray:
-	# create dynamic array
-	li $v0, 9
-	li $a0, 32	# 8 elements at 4 bytes each
-	syscall
-
-	move $s0, $v0	# store array start address
-	li $s1, 8		# store array length
-	
-	# load numbers
-	li $t1, 4
-	li $t2, 1
-	li $t3, 2
-	li $t4, 7
-	li $t5, 3
-	li $t6, 9
-	li $t7, 8
-	li $t8, 6
-	
-	# push numbers into array
-	sw $t1,   ($s0)
-	sw $t2,  4($s0)
-	sw $t3,  8($s0)
-	sw $t4, 12($s0)
-	sw $t5, 16($s0)
-	sw $t6, 20($s0)
-	sw $t7, 24($s0)
-	sw $t8, 28($s0)
-
-printUnsorted:
-	li $v0, 4
-	la $a0, outUnsortedArray
-	syscall
-
-	# print array
-	jal printArray
-
 sortArray:
-	# idea: use the stack for our dynamic array?
-	#	- store i, j, pivot and bounds as copies of $sp
+	# call quick sort algorithm
+	move $a0, $s0
+	move $a1, $s1
+	jal quicksort
 
-printSortedArray:
-	li $v0, 4
-	la $a0, outSortedArray
-	syscall
+	# print sorted array
 	jal printArray
 
 # -----
@@ -188,40 +143,109 @@ endPrintArray:
 .ent quicksort
 quicksort:
 	# variables (will sort arr[a:b])
-	#	- arr: array of numbers
-	#	- a: start index (include)
-	#	- b: end index (exclude)
+	#	- $a0: address of start of array (or section of array)
+	#	- $a1: length of array to sort
 	# calculate:
 	#	- p: pivot (last, for simplicity)
 	#	- i: last element swapped (last element known to be in correct position)
 	#	- j: current element being checked
-	# recursive call:
-	#	- quicksort(arr[a:pivot])
-	#	- quicksort(arr[pivot+1:b])
+
+	# store return address on stack
+	subu $sp, $sp, 4
+	sw $ra, ($sp)
+
+	move $t7, $a0	# store start of array
+	move $t8, $a1	# store length of array
+
+base_case:
+	ble $t8, 1, return	# if length <= 1, return
+
+setup:
+	# $t0 = i (address before first element: $a0 - 4)
+	addi $t0, $t7, -4	# i = start - 4
+
+	# $t1 = j (address of first element: $a0)
+	move $t1, $t7		# j = start of array
+
+	# $t2 = pivot (last element: $t0 offset by length bytes)
+	sll $t2, $t8, 2		# pivot offset = length * 4
+	add $t2, $t0, $t2	# pivot address = (start - 4) + offset
+
+loop:
+	beq $t1, $t2, move_pivot	# if j == pivot, move pivot (end loop)
+
+	# compare arr[j] and arr[pivot]
+	lw $t3, ($t1)	# load arr[j]
+	lw $t4, ($t2)	# load arr[pivot]
+	blt $t3, $t4, swap	# if arr[j] < arr[pivot] -> swap arr[i] and arr[j]
+
+	b post_loop	# if arr[j] >= arr[pivot], continue loop
 
 swap:
+	addi $t0, $t0, 4	# increment i
+	beq $t0, $t1, post_loop	# if i == j, swap does nothing
+
 	# swap arr[i] and arr[j]
+	lw $t3, ($t0)	# load arr[i] into temp1
+	lw $t4, ($t1)	# load arr[j] into temp1
+	sw $t3, ($t1)	# store arr[i] in arr[j]
+	sw $t4, ($t0)	# store arr[j] in arr[i]
+
+post_loop:
+	addi $t1, $t1, 4	# increment j
+	b loop
+
+move_pivot:
+	# swap pivot with arr[i+1]
+	addi $t0, $t0, 4	# increment i
+
+	# swap arr[i+1] and arr[pivot]
+	lw $t3, ($t2)	# load pivot into temp1
+	lw $t4, ($t0)	# load arr[i] into temp2
+	sw $t4, ($t2)	# store arr[i] in last position
+	sw $t3, ($t0)	# store pivot where i was
+
+	# $t0 is now the pivot
+	# $t2 is now the last element of the array slice
 
 recursiveCalls:
-	# recursive calls to quicksort
+	# recursive calls to quicksort:
+	#	- quicksort(arr[start:pivot])
+	#	- quicksort(arr[pivot+1:end])
 
-	# base case: if a >= b, return
+	addi $t3, $t0, 4	# pivot+1 (start of array for second recursive call)
+	sub $t4, $t0, $t7	# pivot_offset = pivot - start
+	div $t4, $t4, 4		# pivot_index = pivot_offset /4
+	sub $t4, $t8, $t4	# length = original_length - pivot_index
+	addi $t4, $t4, -1	# compensate for zero-indexing
 
-	# calculate pivot
-	#	- p = b - 1
-	#	- i = a - 1
-	#	- j = a
+	# store pivot+1 and length on stack for second call
+	subu $sp, $sp, 8
+	sw $t3, ($sp)		# pivot+1
+	sw $t4, 4($sp)		# length
 
-	# loop:
-	#	- if arr[j] < arr[p], swap arr[i+1] and arr[j], increment i
-	#	- increment j
+	# $a0 -> start of array from original call
+	move $a0, $t7
+	# $a1 -> distance from start of array to pivot
+	sub $a1, $t0, $a0	# length (bytes) = pivot - start
+	div $a1, $a1, 4		# length /= 4
 
-	# swap arr[i+1] and arr[p]
+	# recursive call: left partition
+	jal quicksort
 
-	# recursive call: quicksort(arr[a:pivot])
-	# recursive call: quicksort(arr[pivot+1:b])
+	# return array and length from stack
+	lw $a0, ($sp)		# pivot+1
+	lw $a1, 4($sp)		# length
+	addu $sp, $sp, 8
 
-	# return
+	# recursive call: right partition
+	jal quicksort
+
+return:
+	lw $ra, ($sp)
+	addu $sp, $sp, 4
+
+	jr $ra
 
 
 .end quicksort
