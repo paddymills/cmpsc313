c9b8456b730fbd88ee4585f419749cbf9a63ac92
Patrick Miller <paddymills@proton.me>
2024-11-12 22:38:54 -0500

    impl read file

--- src/hw5/person.asm
+++ src/hw5/person.asm
@@ -10,6 +10,8 @@
 # ----------------------------------------
 # Data Declarations:
 .data
+peopleFile: .asciiz "/src/src/hw5/people.txt"
+buffer: .space 32
 
 # ----------------------------------------
 # Program Code:
@@ -20,8 +22,39 @@
 main:
 # -----
 # Program start
+	# open file
+	li $v0, 13
+	la $a0, peopleFile
+	li $a1, 0
+	syscall
+	move $s0, $v0
 
+	li $t0, 0
+loop_read:
+	# read line
+	li $v0, 14
+	move $a0, $s0
+	la $a1, buffer
+	li $a2, 4
+	syscall
+	move $t0, $v0	# bytes read (0 = EOF, -1 = error)
 
+	li $v0, 1
+	move $a0, $t0
+	syscall
+
+	blez $t0, end_read
+
+	li $v0, 4
+	la $a0, buffer
+	syscall
+
+	j loop_read
+
+end_read:
+	li $v0, 16
+	move $a0, $s0
+	syscall
 
 # -----
 # Done, terminate program.
