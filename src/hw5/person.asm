7a5f51c5e19ad3a0111838a2af3b5128e76a813e
Patrick Miller <paddymills@proton.me>
2024-12-03 21:36:00 -0500

    person not working

--- src/hw5/person.asm
+++ src/hw5/person.asm
@@ -11,7 +11,12 @@
 # Data Declarations:
 .data
 peopleFile: .asciiz "/src/src/hw5/people.txt"
-buffer: .space 32
+bufferFirst: .space 32
+bufferLast: .space 32
+buffer: .space 64
+
+testFirst: .asciiz "Henry"
+testLast: .asciiz "Smith"
 
 # ----------------------------------------
 # Program Code:
@@ -22,6 +27,12 @@ buffer: .space 32
 main:
 # -----
 # Program start
+	li $v0, 1
+	li $a0, 0
+	syscall
+
+	j test
+
 	# open file
 	li $v0, 13
 	la $a0, peopleFile
@@ -56,6 +67,17 @@ end_read:
 	move $a0, $s0
 	syscall
 
+test:
+	la $a0, testFirst
+	la $a1, testLast
+	jal Person
+	move $s0, $v0
+
+	# print Person
+	move $a0, $s0
+	lw $t0, 0($s0)
+	jalr $t0
+
 # -----
 # Done, terminate program.
 end:
@@ -63,3 +85,53 @@ end:
 	syscall
 
 .end main
+
+.globl Person
+.ent Person
+Person:
+	# assume the first name is in $a0 and last name is in $a1
+	la $t0, toString
+	move $t1, $a0
+	move $t2, $a1
+
+	# allocate object dynamically
+	li $v0, 9
+	li $a0, 12
+	syscall
+
+	# build object jump table
+	sw $t0, 0($v0)
+	sw $t1, 4($v0)
+	sw $t2, 8($v0)
+
+	jr $ra
+
+.end Person
+
+.text
+toString:
+	# assume the Person object is in $a0
+	lw $t0, 4($a0)	# get first name
+	lw $t1, 8($a0)	# get last name
+
+	li $v0, 4
+	la $a0, ($t0)
+	syscall
+
+	li $v0, 11
+	li $a0, 0x20	# ascii for space (' ')
+	syscall
+
+	li $v0, 4
+	la $a0, ($t1)
+	syscall
+
+	# print "\n" (line feed + carriage return)
+	li $v0, 11
+	li $a0, 0x0A
+	syscall
+	li $v0, 11
+	li $a0, 0x0D
+	syscall
+
+	jr $ra
